1c0f158df63de93ee1f4e31f05ecf7b534b5f110
KarenMorisha <ekarenmorisha23@gmail.com>
2025-07-21 14:28:29 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,9 @@
+public class Ternary{
+    public static void main(String[] args) {
+        String Car = "Ritz";
+        boolean domestic = Car == "Ritz" ? false : true;
+        if(domestic){
+            System.out.println("Domestic");
+        }
+    }
+}
